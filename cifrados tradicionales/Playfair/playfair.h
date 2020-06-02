70b5f90a22b99eaa512a1ba03c3bad5cac7a02fe
javier.alvarez Reyes <40539959+6162636465@users.noreply.github.com>
2020-06-01 21:12:27 -0500

    Update playfair.h
    avanse para el cifrado 1.0

--- cifrados tradicionales/Playfair/playfair.h
+++ cifrados tradicionales/Playfair/playfair.h
@@ -18,6 +18,8 @@ public:
   string nuevoAlfabeto(string);
   string Alfabeto();
   void decifrar();
+  string separar(string,int,int);
+  string allar(int,int,string,string,string,string,string);
 };
 cifrado::cifrado(string _x,string _m,string _u)
 {
@@ -49,8 +51,7 @@ string cifrado::Alfabeto()
   return aiuda;
 }
 
-string cifrado::nuevoAlfabeto(string A)
-{
+string cifrado::nuevoAlfabeto(string A){
   string aiuda=A;
   for(int i=0;i<u.length();i++)
   {
@@ -72,9 +73,61 @@ string cifrado::nuevoAlfabeto(string A)
   }
   return aiuda;
 }
+string cifrado::separar(string rito,int x, int y){
+  string relleno;
+  for(x; x<y; x++){
+    relleno+=rito[x];
+    }
+  return relleno;
+} 
+string cifrado::allar(int x, int y,string A,string B,string C,string D,string E)
+{
+  if( (x>=0 && x<5) || (y<5 && y>=0))
+  {
+
+  }
+  if( (x>=5 && x<10) || (y<10 && y>=0))
+  {
+    
+  }
+  if( (x>=10 && x<15) || (y<15 && y>=0))
+  {
+    
+  }
+  if( (x>=15 && x<20) || (y<20 && y>==0))
+  {
+    
+  }
+  if( (x>=20 && x<25) || (y<25 && y>===0))
+  {
+    
+  }
+}
 void cifrado::cifrar()
 {
   string alfabeto2=Alfabeto();//quitarle las letras repetidas
   string alfabeto3=nuevoAlfabeto(alfabeto2);
-  cout<<alfabeto3<<"tamaño ="<<alfabeto3.length()<<endl;
+  cout<<alfabeto3<<endl<<"tamaño ="<<alfabeto3.length()<<endl;
+  string itachi=separar(alfabeto3,0,5);cout<<itachi<<endl;
+  string obito=separar(alfabeto3,5,10);cout<<obito<<endl;
+  string picoro=separar(alfabeto3,10,15);cout<<picoro<<endl;
+  string goku=separar(alfabeto3,15,20);cout<<goku<<endl;
+  string pikachu=separar(alfabeto3,20,25);cout<<pikachu<<endl;
+  int terminato=0, gaaaaaaaaa=0;//contador xdxd
+  int temp,temp1;
+  int yara;
+  for(int i=0;i<m.length();i=i+2)
+  {
+    for(int j=0;j<alfabeto3.length();j++)
+    {
+      if(m[i]==alfabeto3[j])
+      {
+      temp=alfabeto3.find(m[i]);
+      }
+      if(m[i+1]==alfabeto3[j]){
+      temp1=alfabeto3.find(m[i+1]);
+      }
+    }
+    allar(temp,temp1,itachi,obito,picoro,goku,pikachu);
+  }
 }
